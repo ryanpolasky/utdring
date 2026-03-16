e21fd716a7813d85dc0817020d255b426fe46a16
Arin101230523 <AXS230135@utdallas.edu>
2026-03-16 13:15:04 -0500

    Shuffles order daily

---
+++ javascript/shuffle.js
@@ -0,0 +1,24 @@
+function seededRandom(seed) {
+  let s = seed;
+  return () => {
+    s = (s * 1664525 + 1013904223) & 0xffffffff;
+    return (s >>> 0) / 0xffffffff;
+  };
+}
+
+function getDailySeed() {
+  const now = new Date();
+  return now.getFullYear() * 10000 + (now.getMonth() + 1) * 100 + now.getDate();
+}
+
+function shuffleWithSeed(array, seed) {
+  const rand = seededRandom(seed);
+  const arr = [...array];
+  for (let i = arr.length - 1; i > 0; i--) {
+    const j = Math.floor(rand() * (i + 1));
+    [arr[i], arr[j]] = [arr[j], arr[i]];
+  }
+  return arr;
+}
+
+webringData.sites = shuffleWithSeed(webringData.sites, getDailySeed());
